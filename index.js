ecd92ca83e97fa8f6b2b6708f6d0b0540fecad91
Rose Yemelyanova <rose.yemelyanova@diamond.ac.uk>
2022-04-05 14:33:14 +0100

    modified connection string and tests to pass test:module2

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = 1025;
 
 //mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://127.0.0.1:27017`, {
+mongoose.connect('mongodb://localhost:27017/productsdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
